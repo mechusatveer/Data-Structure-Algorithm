4cb3161406019a77092d337382114df28667b0ce
Satveer Singh Mechu <mechu.satveer@gmail.com>
2014-12-21 00:57:31 +0530

    Create PaintARowOfHouses.cpp

---
+++ DP/PaintARowOfHouses.cpp
@@ -0,0 +1,14 @@
+/*
+MINIMUM COST FOR PAINTING A ROW OF HOUSES IN THREE DIFFERENT COLOURS
+   
+There are N houses in a row. Each house can be painted in either Red,Green or Blue colour. 
+The cost of colouring each house in each of the colours is different.
+
+Find the colour of each house such that no two adjacent house have the same colour and the total 
+cost of colouring all the houses is minimum.The question intends to state that cost of painting 
+any house in any colour is different, so if cost of painting House 1 in Red is say, X then the 
+cost of painting House 2 in red will some other value Y. It can be considered each house has 
+different dimensions and hence cost of painting in each colour is different, and the cost of paint
+for each house also varies.
+
+*/
