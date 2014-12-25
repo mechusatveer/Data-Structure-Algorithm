92be617d5150b25689d26fcee94708dbe77e608e
Satveer Singh Mechu <mechu.satveer@gmail.com>
2014-12-25 17:17:30 +0530

    Update MaxDiffPair.cpp

--- Array/MaxDiffPair.cpp
+++ Array/MaxDiffPair.cpp
@@ -1,60 +1,42 @@
 //Print Max Diff Pair
-
-
+// 1) arr[i] < arr[j] & i < j
+// 2) No constraint like that 
 #include<iostream>
 
 using namespace std;
 
-int Min(int i, int j)
+int PrintPair_1(int arr[], int n)
 {
-    if(i < j) return i;
-    return j;
-}
-
-int Max(int i, int j)
-{
-    if(i > j) return i;
-    return j;
-}
-int PrintPair(int arr[], int n)
-{
-   int* temp_min = new int[n];//This array hold min item upto here
-   int* temp_max = new int[n];//This array hold max item upto here
-
-   temp_min[0] = arr[0];
-   temp_max[0] = arr[0];
    
    int min = temp[0];
-   int max = temp[0];
+  
+   int max_diff = 0;
    
-   for(int i = 1; i < n-1; i++)
+   for(int i = 1; i < n; i++)
    {
+       max_diff = Max(max_diff,arr[i]-min)
        min = Min(min,arr[i]);
-       max = Max(max,arr[i]);
-       temp_min[i] = min;
-       temp_max[i] = max;
    }
-
-   min = arr[n-1];
-   max = arr[n-1];
-   
-   int max_Diff = 0;
-   
-   int curr_Diff = 0;
-   for(int i = n-2; i >= 0; --i)
-   {
-      curr_Diff = Max(max - temp_min[i], min - temp_max[i]);
-      if(curr_Diff > max_Diff) max_Diff = curr_Diff;
-      
-      max = Max(max,arr[i]);
-      min = Min(min,arr[i]);
-   }
-
-   delete []temp;
-   temp = NULL;
    return max_Diff;
 }
 
+int PrintPair_2(int arr[], int n)
+{
+   
+   int min = temp[0];
+   int max = temp[0];
+  
+   int max_diff = 0;
+   
+   for(int i = 1; i < n-1; i++)
+   {
+       max_diff = Max(max_diff,arr[i]-min);
+       max_diff = Max(max_diff,max-arr[i]);
+       min = Min(min,arr[i]);
+       max = Max(max,arr[i]);
+   }
+   return max_Diff;
+}
 int main()
 {
    int arr[] = {7,9,5,6,3,2};
