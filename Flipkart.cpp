b92dacdb8627f3e18cf12e8904b3c268a71abd50
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-31 12:49:57 +0530

    Create Flipkart.cpp

---
+++ Flipkart.cpp
@@ -0,0 +1,41 @@
+1) You have n requests from different people to book a room
+    In a year think days are 0-364
+    Req1  3-7 mean i want to book a room from day 3 to day 7
+    Req2  2-19 mean i want to book a room from day 2 to day 19
+    Req3  5-7 mean i want to book a room from day 5 to day 7
+
+   Now all request cannot be processed
+   We want 
+
+  1) How many max request can be processsed
+  2) How much max rent we can get it mean how many max days u can keep your room busy
+
+  request cannot accepted partial
+
+ 1-2
+ 3-4
+ 1-5
+
+ Here how many max request we can process - 2  1-2 & 3-4
+But how many max days we can keep busy this room is 1-5 and in that case only 1 req be processed.
+
+Any doubt drop a mail to me
+
+2) You are school teacher, u have your n students
+    You have one array of their marks and all these n students are in a queu
+    Then can know marks of next and prev student
+    
+    Now condition is u need to distribute sweets between these students
+    If a student have more marks then any neighbour he want more sweets than that neighbour
+
+    Note:- we not say equal
+    3 4 4
+    1 2 1 its ok 
+     3rd cannot say why i not get two bcoz he not have more marks 
+
+    Simply say you can make a argument if you have more marks than your neighbour
+
+   Now we need to distrubite sweets such a way 
+   So no argument happen there
+   and sweets used be less.
+-------------------------------------------------------------------------------------------------------
