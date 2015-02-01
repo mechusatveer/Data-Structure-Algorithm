a37f0e31c8241cab28e76f5e57c4778235770e48
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-01 17:38:13 +0530

    Create Hanoi.cpp

---
+++ Recursion/Hanoi.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+void Hanoi(char source, char mid, char dest, int n)
+{
+   if(n == 0) return;
+   if(n == 1)
+   {
+        cout<<"Shift "<<n<<"From "<<source<<"To "<<dest<<endl;
+        return;
+   }
+
+   Hanoi(source,dest,mid,n-1);
+   cout<<"Shift "<<n<<"From "<<source<<"To "<<dest<<endl;
+
+   Hanoi(mid,dest,source,n-2);
+   cout<<"Shift "<<n-1<<"From "<<mid<<"To "<<dest<<endl;
+
+   Hanoi(source,mid,dest,n-2); // do not forget to write this mean we solving a problem with n-2 items.
+}
+
+int main()
+{
+   int n = 3;
+   Hanoi('A','B','C',n);
+   return 0;
+}
