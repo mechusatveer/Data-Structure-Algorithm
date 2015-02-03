65c950edae74a179e59a32c628c2d5329433ea4a
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-03 12:36:47 +0530

    Update Hanoi.cpp

--- Recursion/Hanoi.cpp
+++ Recursion/Hanoi.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void Hanoi(char source, char mid, char dest, int n)
+void Hanoi_1(char source, char mid, char dest, int n)
 {
    if(n == 0) return;
    if(n == 1)
@@ -18,6 +18,22 @@ void Hanoi(char source, char mid, char dest, int n)
    cout<<"Shift "<<n-1<<"From "<<mid<<"To "<<dest<<endl;
 
    Hanoi(source,mid,dest,n-2); // do not forget to write this mean we solving a problem with n-2 items.
+}
+
+void Hanoi_2(char source, char mid, char dest, int n)
+{
+   if(n == 0) return;
+   if(n == 1)
+   {
+        cout<<"Shift "<<n<<"From "<<source<<"To "<<dest<<endl;
+        return;
+   }
+
+   Hanoi(source,dest,mid,n-1);
+   cout<<"Shift "<<n<<"From "<<source<<"To "<<dest<<endl;
+
+   Hanoi(mid,source,dest,n-1);
+  
 }
 
 int main()
